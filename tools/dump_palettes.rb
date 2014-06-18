eaae74d760d7976fdd1a8b04fd60e5bd5cc9a5a5
Carmen Wick <CarmenWick@gmail.com>
2014-06-18 13:59:51 -0700

    Dump palettes

---
+++ tools/dump_palettes.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+PALETTE_COUNT = 23
+
+open("../data/palette.dat", "rb") do |f|
+  palette_count = f.size / (256*3)
+  palettes = []
+
+  raise "Bad palette count" unless palette_count == PALETTE_COUNT
+
+  palette_count.times do
+    palette = []
+
+    256.times do
+      color = {}
+      color[:r] = f.getbyte << 2
+      color[:g] = f.getbyte << 2
+      color[:b] = f.getbyte << 2
+
+      palette << color
+    end
+
+    palettes << palette
+  end
+
+  File.open("out/palettes.json", "w") { |f| f.write JSON.pretty_generate(palettes) }
+end
+
+
+
